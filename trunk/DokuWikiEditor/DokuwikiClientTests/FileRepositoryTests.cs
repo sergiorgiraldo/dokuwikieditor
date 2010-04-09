37ef41d8db6e86caf83df9e08b09600b6ab8d3f8
froorider@gmail.com <froorider@gmail.com@168fb96a-f07e-11de-88d6-afc75ad17f39>
2010-04-09 14:19:11 +0000

    did some modifications on FileRepository

---
+++ trunk/DokuWikiEditor/DokuwikiClientTests/FileRepositoryTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DokuwikiClient.Domain.Entities;
+using CH.Froorider.DokuwikiClient.Persistence;
+
+namespace DokuWikiClientTests
+{
+	[TestClass]
+	public class FileRepositoryTests
+	{
+		[TestMethod]
+		public void Store_StoreAWikiAccount_BusinessObjectShouldBeStoredWithoutErrors()
+		{
+			Wikipage page = new Wikipage();
+			page.WikiPageContent = "ölkjölkfsajdölkjafsd";
+			page.WikiPageName = "start.php";
+
+			FileRepository repository = new FileRepository();
+			string identifier = repository.Store<Wikipage>(page);
+			Assert.IsFalse(String.IsNullOrEmpty(identifier));
+			Assert.AreEqual(identifier, page.ObjectIdentifier);
+		}
+
+		[TestMethod]
+		public void GetIdentifiers_StandardProcedure_AListOfStringsIsReturned()
+		{
+			WikiAccount account = new WikiAccount();
+			account.AccountName = "foobar";
+			account.LoginName = "barfoo";
+			account.Password = "A secret password";
+			account.WikiUrl = new Uri("http://some.where.over/the/Rainbow");
+
+			FileRepository repository = new FileRepository();
+			string targetIdentifier = repository.Store<WikiAccount>(account);
+
+			foreach (string identifier in repository.GetIdentifiers())
+			{
+				Assert.IsFalse(String.IsNullOrEmpty(identifier));
+				Assert.AreEqual(targetIdentifier, identifier);
+			}
+		}
+	}
+}
