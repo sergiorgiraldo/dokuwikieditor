a5b2e3056459250cdfcbe5234473b86e2d150d1d
froorider <froorider@gmail.com>
2010-04-16 15:59:50 +0000

    added some more testing

--- DokuWikiEditor/DokuwikiClientTests/FileRepositoryTests.cs
+++ DokuWikiEditor/DokuwikiClientTests/FileRepositoryTests.cs
@@ -12,7 +12,7 @@ namespace DokuWikiClientTests
 	public class FileRepositoryTests
 	{
 		[TestMethod]
-		public void Store_StoreAWikiAccount_BusinessObjectShouldBeStoredWithoutErrors()
+		public void Store_StoreAWikiPage_BusinessObjectShouldBeStoredWithoutErrors()
 		{
 			Wikipage page = new Wikipage();
 			page.WikiPageContent = "ölkjölkfsajdölkjafsd";
@@ -41,6 +41,40 @@ namespace DokuWikiClientTests
 				Assert.IsFalse(String.IsNullOrEmpty(identifier));
 				Assert.AreEqual(targetIdentifier, identifier);
 			}
+		}
+
+		[TestMethod]
+		public void Load_LoadAWikiAccount_IsLoadedWithoutErrors()
+		{
+			WikiAccount expectedAccount = new WikiAccount();
+			expectedAccount.AccountName = "doodle";
+			expectedAccount.LoginName = "Dödel";
+			expectedAccount.Password = "Very secret password";
+			expectedAccount.WikiUrl = new Uri("http://www.doodle.ch/tinyurl/349");
+
+			FileRepository repository = new FileRepository();
+			string targetIdenitfier = repository.Store<WikiAccount>(expectedAccount);
+
+			WikiAccount targetAccount = repository.Load<WikiAccount>(targetIdenitfier);
+			Assert.AreEqual(targetAccount, expectedAccount);
+			Assert.IsTrue(targetAccount.Equals(expectedAccount));
+		}
+
+		[TestMethod]
+		public void Store_StoreAModifiedWikiAccount_IdentifierShouldStayTheSame()
+		{
+			Wikipage pageToStore = new Wikipage();
+			pageToStore.WikiPageName = "first.php";
+			pageToStore.WikiPageContent = "Lorem ipsum dolor sit amet";
+
+			FileRepository repository = new FileRepository();
+			string firstIdentifier = repository.Store<Wikipage>(pageToStore);
+
+			pageToStore.WikiPageContent = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr," +
+				" sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.";
+			string secondIdentifier = repository.Store<Wikipage>(pageToStore);
+
+			Assert.AreEqual(firstIdentifier, secondIdentifier);
 		}
 	}
 }
